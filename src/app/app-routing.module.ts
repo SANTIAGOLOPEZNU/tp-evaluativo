bba01da59c0f11e867e5d4735c01f13927852bb7
SANTIAGOLOPEZNU <sanlopeznunez@gmail.com>
2024-06-24 09:48:07 -0300

    creamos la estructura basica con models, modules, carpetas y componentes y vinculamos algunas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //ruta predeterminada en inicio
+  {
+    path:"",component:InicioComponent
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/inicio//inicio.module').then(m=>m.InicioModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
